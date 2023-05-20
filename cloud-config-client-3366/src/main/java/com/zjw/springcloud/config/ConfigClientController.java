8cc65ae6ce680b1518b5bd919bd0b60505bbd642
zjw <2827557475@qq.com>
2023-05-20 19:01:40 +0800

    阿伟的学习笔记

--- cloud-config-client-3366/src/main/java/com/zjw/springcloud/config/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/zjw/springcloud/config/ConfigClientController.java
@@ -1,21 +1,24 @@
-package com.zjw.springcloud.controller;
+package com.zjw.springcloud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RefreshScope
 public class ConfigClientController
 {
+    @Value("${server.port}")
+    private String serverPort;
+
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
-    public String getConfigInfo()
+    public String configInfo()
     {
-        return configInfo;
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
     }
+
 }
